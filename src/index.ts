37e95efeaf66f644eb5bac13ae6fc78f7ea6bb6a
Leon564 <nleon564.nl@gmail.com>
2023-07-07 00:20:00 -0600

    refactor(app): Update project structure for lyrics data

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,4 @@
-import axios from "axios";
-import * as cheerio from 'cheerio';
-import { SearchData } from './types';
-
-const options = {
-  headers: {
-    "user-agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4918.0 Safari/537.36",
-    referer: "https://www.karaoke-lyrics.net/",
-  },
-};
-
-async function search(query: string) : Promise<SearchData | null>{
-  const searchUrl = `https://www.karaoke-lyrics.net/search?sid=jyptz&q=${encodeURIComponent(query)}`;
-
-  const htmls = await axios.get(searchUrl, options);
-  const $ = cheerio.load(htmls.data);
-
-  const spanSSong = $("span.searchresrow_songs").eq(0);
-  if (!spanSSong.length) return null;
-
-  const aSSong = spanSSong.children().eq(0);
-  const songUrl = `https://www.karaoke-lyrics.net${aSSong.attr('href')}`;
-
-  const htmlSong = await axios.get(songUrl, options);
-  const $song = cheerio.load(htmlSong.data);
-
-  const artist = $song("div.navigation_lyrics").children().eq(2).text().trim();
-
-  const h1sTitle = $song("h1#song_title").text();
-  const title = h1sTitle.replace("- lyrics", "").trim();
-
-  const lyrics = $song("div.para_row")
-    .map((i, el) => $song(el).text().trim())
-    .get()
-    .join("\n\n");
-
-    const data: SearchData = { title, artist, lyrics };
-  return data;
-}
+import search from './lyricsLibrary';
+export { SearchData } from './types';
 
 export default search;
